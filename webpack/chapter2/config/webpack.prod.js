e133f64291d48734cdbb13d66627912c4a6d0b96
“y-small-space” <jy.small.space@outlook.com>
2023-12-11 04:10:12 +0800

    .

--- webpack/chapter2/config/webpack.prod.js
+++ webpack/chapter2/config/webpack.prod.js
@@ -1,49 +1,62 @@
-// Node.js的核心模块，专门用来处理文件路径
 const path = require("path");
-const ESLintPlugin = require('eslint-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintWebpackPlugin = require("eslint-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+
+// 获取处理样式的Loaders
+const getStyleLoaders = (preProcessor) => {
+  return [
+    MiniCssExtractPlugin.loader,
+    "css-loader",
+    {
+      loader: "postcss-loader",
+      options: {
+        postcssOptions: {
+          plugins: [
+            "postcss-preset-env", // 能解决大多数样式兼容性问题
+          ],
+        },
+      },
+    },
+    preProcessor,
+  ].filter(Boolean);
+};
 
 module.exports = {
-  // 入口
-  // 相对路径和绝对路径都行
   entry: "./src/main.js",
-  // 输出
   output: {
-    // path: 文件输出目录，必须是绝对路径
-    // path.resolve()方法返回一个绝对路径
-    // __dirname 当前文件的文件夹绝对路径
-    path: path.resolve(__dirname, "../dist"), // 绝对路径
-    filename: "static/js/main.js",
-    clean: true
+    path: path.resolve(__dirname, "../dist"), // 生产模式需要输出
+    filename: "static/js/main.js", // 将 js 文件输出到 static/js 目录中
+    clean: true,
   },
-  // 加载器
   module: {
     rules: [
       {
         // 用来匹配 .css 结尾的文件
         test: /\.css$/,
         // use 数组里面 Loader 执行顺序是从右到左
-        use: ["style-loader", "css-loader"],
+        use: getStyleLoaders()
       },
       {
         test: /\.less$/,
-        use: ["style-loader", "css-loader", "less-loader"],
+        use: getStyleLoaders("less-loader")
       },
       {
         test: /\.s[ac]ss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: getStyleLoaders("sass-loader"),
       },
       {
         test: /\.styl$/,
-        use: ["style-loader", "css-loader", "stylus-loader"],
+        use: getStyleLoaders("stylus-loader"),
       },
       {
         test: /\.(png|jpe?g|gif|webp)$/,
         type: "asset",
         parser: {
           dataUrlCondition: {
-            maxSize: 10 * 1024 // 小于10kb的图片会被base64处理
-          }
+            maxSize: 10 * 1024, // 小于10kb的图片会被base64处理
+          },
         },
         generator: {
           // 将图片文件输出到 static/imgs 目录中
@@ -55,15 +68,7 @@ module.exports = {
         },
       },
       {
-        // 处理字体图标资源
         test: /\.(ttf|woff2?)$/,
-        type: "asset/resource",
-        generator: {
-          filename: "static/media/[hash:8][ext][query]",
-        },
-      },
-      {
-        test: /\.(ttf|woff2?|map4|map3|avi)$/,
         type: "asset/resource",
         generator: {
           filename: "static/media/[hash:8][ext][query]",
@@ -76,10 +81,9 @@ module.exports = {
       },
     ],
   },
-  // 插件
   plugins: [
-    // plugin的配置
-    new ESLintPlugin({
+    new ESLintWebpackPlugin({
+      // 指定检查文件的根目录
       context: path.resolve(__dirname, "../src"),
     }),
     new HtmlWebpackPlugin({
@@ -87,13 +91,17 @@ module.exports = {
       // 新的html文件有两个特点：1. 内容和源文件一致 2. 自动引入打包生成的js等资源
       template: path.resolve(__dirname, "../public/index.html"),
     }),
+    // 提取css成单独文件
+    new MiniCssExtractPlugin({
+      // 定义输出文件名和目录
+      filename: "static/css/main.css",
+    }),
+    new CssMinimizerPlugin(),
   ],
-  // 开发服务器
-  devServer: {
-    host: "localhost", // 启动服务器域名
-    port: "3000", // 启动服务器端口号
-    open: true, // 是否自动打开浏览器
-  },
-  // 模式
-  mode: "production", // 开发模式
+  // devServer: {
+  //   host: "localhost", // 启动服务器域名
+  //   port: "3000", // 启动服务器端口号
+  //   open: true, // 是否自动打开浏览器
+  // },
+  mode: "production",
 };
