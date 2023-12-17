bbd35ca90c1f9230c96727645361ea547b69f874
“y-small-space” <jy.small.space@outlook.com>
2023-12-18 02:09:37 +0800

    ejs

--- nodejs/笔记/01-expres模块/src/12-图片防盗链.js
+++ nodejs/笔记/01-expres模块/src/12-图片防盗链.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const app = express();
 
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
+  // 检测请求头中的 referer 是否为 127.0.0.1
+  // 获取 referer
   let referer = req.get('referer');
-  let url = new URL(referer);
-  let hostname = url.hostname;
-
-  if(hostname !== '127.0.0.1'){
-    res.status
+  if (referer) {
+    // 实例化
+    let url = new URL(referer);
+    // 获取 hosthome
+    let hostname = url.hostname;
+    // 判断
+    if (hostname !== '127.0.0.1') {
+      res.status(404).send(`<h1>404 Not Found</h1>`);
+    }
   }
   console.log(host);
   next();
+})
+
+app.listen(3000, () => {
+  console.log('server on ....');
 })
